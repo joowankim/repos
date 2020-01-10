005a985d8f7cec73e6721c3aec5612c3ec5bb652
kjw217 <rlawndhks217@gmail.com>
2020-01-11 02:56:29 +0900

    custom field

--- myapp/item/models.py
+++ myapp/item/models.py
@@ -1,35 +1,31 @@
 from django.db import models
 
-# Create your models here.
-# class Item(models.Model):
-#     name = models.CharField(max_length=32)
-#     quantity = models.IntegerField()
-
 class Ingredient(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=256)
+    name = models.CharField(primary_key=True, max_length=200)
     oily = models.IntegerField()
     dry = models.IntegerField()
     sensitive = models.IntegerField()
 
+    def __unicode__(self):
+        return '{}'.format(self.name)
+
 class Item(models.Model):
     id = models.IntegerField(primary_key=True)
-    imageId = models.CharField(max_length=256)
     name = models.CharField(max_length=256)
     price = models.IntegerField()
     gender = models.CharField(max_length=10)
     category = models.CharField(max_length=50)
-    ingredients = models.CharField(max_length=512)
-    ingredient_ids = models.ManyToManyField(
+    ingredients = models.ManyToManyField(
         Ingredient,
         through='Included',
         through_fields=('item', 'ingredient'),
+        related_name='ingredients'
     )
     monthlySales = models.IntegerField()
     oily = models.IntegerField()
     dry = models.IntegerField()
     sensitive = models.IntegerField()
-    image = models.CharField(max_length=512)
+    full = models.CharField(max_length=512)
     thumb = models.CharField(max_length=512)
 
 class Included(models.Model):
--- myapp/item/serializers.py
+++ myapp/item/serializers.py
@@ -1,44 +1,27 @@
 from rest_framework import serializers
 from .models import Ingredient, Item, Included
-#
-# class IngredientSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Ingredient
-#         fields = '__all__'
-#
-# class ItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Item
-#         fields = '__all__'
-#
-# class IncludedSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Included
-#         fields = '__all__'
 
-# class ProductsSerializer(serializers.Serializer):
-#     id = serializers.IntegerField()
-#     imgUrl = serializers.CharField(max_length=512)
-#     name = serializers.CharField(max_length=256)
-#     price = serializers.IntegerField()
-#     ingredients = serializers.CharField(max_length=512)
-#     monthlySales = serializers.IntegerField()
+class IngredientsField(serializers.Field):
+    def to_representation(self, value):
+        return ','.join([ingredient.name for ingredient in value.all()])
 
 class ProductsSerializer(serializers.ModelSerializer):
     imgUrl = serializers.SerializerMethodField('get_imgUrl')
+    ingredients = IngredientsField()
     class Meta:
         model = Item
         fields = ('id', 'imgUrl', 'name', 'price', 'ingredients', 'monthlySales')
     def get_imgUrl(self, obj):
-        return obj.image
+        return obj.full
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     imgUrl = serializers.SerializerMethodField('get_imgUrl')
+    ingredients = IngredientsField()
     class Meta:
         model = Item
         fields = ('id', 'imgUrl', 'name', 'price', 'gender', 'category', 'ingredients', 'monthlySales')
     def get_imgUrl(self, obj):
-        return obj.image
+        return obj.full
 
 class RecommProductsSerializer(serializers.ModelSerializer):
     imgUrl = serializers.SerializerMethodField('get_imgUrl')
