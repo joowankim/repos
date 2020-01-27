d8bb85b940391df5cc283b3fc5d74a6ac875b4cf
kjw217 <rlawndhks217@gmail.com>
2020-01-28 01:15:23 +0900

    edit empty view

--- myapp/item/views.py
+++ myapp/item/views.py
@@ -11,17 +11,18 @@ from .serializers import ProductsSerializer, ProductDetailSerializer, RecommProd
 
 def empty_view():
     '''
-    skin_type을 입력하지 않은 경우 404에러를 보내기 위한 함수
+    skin_type을 제대로 입력하지 않은 경우 404에러를 보내기 위한 함수
     :return: status 404
     '''
-    content = {'please type <skin_type> on URL': 'nothing to see here'}
+    content = {'please type right <skin_type> on URL': 'nothing to see here',
+               'skin types': 'oily, dry, sensitive'}
     return Response(content, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET'])
 def products_list(request):
     if request.method == 'GET':
         skin_type = request.GET.get('skin_type')
-        if not skin_type:
+        if not skin_type or not skin_type in ['oily', 'dry', 'sensitive']:
             return empty_view()
         category = request.GET.get('category')
         exclude_ingredient = request.GET.get('exclude_ingredient')
@@ -60,7 +61,7 @@ def products_list(request):
 def product_detail(request, id):
     if request.method == 'GET':
         skin_type = request.GET.get('skin_type')
-        if not skin_type:
+        if not skin_type or not skin_type in ['oily', 'dry', 'sensitive']:
             return empty_view()
         product_detail = Item.objects.get(id=id)
         recomms = Item.objects.filter(category=product_detail.category).order_by('-'+skin_type, 'price')[:3]
